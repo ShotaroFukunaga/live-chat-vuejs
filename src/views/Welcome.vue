f53f0d9a75e8ce273782121290ab0077fe34df8f
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-03-21 17:54:11 +0900

    [create]Welocome_view

---
+++ src/views/Welcome.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>{{ 3 + 3 }}</h2>
+    <p v-if="isEnabled">{{ subtitle }}</p>
+    <button @dblclick="toggle">トグルする</button>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      title: '初めてのVue.jsアプリです！',
+      subtitle: 'ようこそ',
+      isEnabled: true
+    }
+  },
+
+  computed: {
+    text(){
+      if(this.isEnabled){
+        return 'こんにちは！'
+      }else{
+        return 'さようなら、、'
+      }
+    }
+  },
+
+  methods: {
+    toggle(){
+      this.isEnabled =! this.isEnabled
+    }
+  }
+}
+</script>

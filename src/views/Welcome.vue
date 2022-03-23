0c1fde30eaf0ddb2cbf756992b741e861383b689
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-03-23 13:33:15 +0900

    [create]Chatroom

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,15 +2,28 @@
 <template>
   <div class="container welcome">
     <p>ようこそ！</p>
-    <Login-Form/>
+      <div v-if="shouldShowLoginForm">
+        <LoginForm/>
+        <p class="change-form">初めての方は<span @click="shouldShowLoginForm = false">こちら</span>をクリック</p>
+      </div>
+      <div v-if="!shouldShowLoginForm">
+        <SignupForm/>
+        <p class="change-form">アカウントをお持ちの方は<span @click="shouldShowLoginForm = true">こちら</span>をクリック</p>
+      </div>
   </div>
 </template>
 
 <script>
-
 import LoginForm from '../components/LoginForm.vue'
+import SignupForm from '../components/SignupForm.vue'
 export default{
-  components: { LoginForm },
+  components: { LoginForm, SignupForm },
+
+  data(){
+    return{
+      shouldShowLoginForm: true
+    }
+  }
 }
 </script>
 
@@ -44,6 +57,11 @@ export default{
   }
   .welcome button{
     margin: 20px auto;
+  }
+
+  .chenge-form{
+    font-size: 14px;
+    margin: 10px;
   }
 
 </style>
